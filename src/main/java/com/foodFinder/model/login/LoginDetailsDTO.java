577e8daedc783664c3bd57979497f41680b30769
Patrycja1986 <55314242+Patrycja1986@users.noreply.github.com>
2020-12-12 12:06:59 +0100

    added DTO class for login details

--- src/main/java/com/foodFinder/model/login/LoginDetailsDTO.java
+++ src/main/java/com/foodFinder/model/login/LoginDetailsDTO.java
@@ -1,4 +1,40 @@
 package com.foodFinder.model.login;
 
 public class LoginDetailsDTO {
+    private Long id;
+    private Long version;
+    private String email;
+    private char password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public char getPassword() {
+        return password;
+    }
+
+    public void setPassword(char password) {
+        this.password = password;
+    }
 }
